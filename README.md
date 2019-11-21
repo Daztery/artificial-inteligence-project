838b970fd5c2d91ee18aab63fa1f2fd2c3920b62
DiegoAloslla <diegoalosilla@gmail.com>
2019-11-21 05:32:38 -0500

    create proyect

---
+++ README.md
@@ -0,0 +1,71 @@
+
+# IA-TB1
+
+  
+
+Trabajo 1 para el curso de Inteligencia Artificial
+
+  
+
+## Dependencias:
+
+  
+- Control de versiones - Git
+- Python 3
+
+  
+
+- Bibliotecas: Pygame, Colorama
+
+  
+
+  
+
+#### Para verificar las versiones:
+
+  
+
+```
+> git --version
+> python --version
+```
+
+  
+
+## Instrucciones
+
+
+Clonar el repositorio con el código fuente:
+```
+> git clone https://github.com/manuxo/IA-TB1.git
+```
+  
+
+Instalar pygame y colorama con el siguiente comando
+
+  
+
+```
+> pip install pygame
+> pip install colorama
+
+```
+
+Una vez instaladas todas las dependencias nos dirigimos a la ruta donde se encuentre el archivo app .py y ejecutamos con el interprete:
+
+
+```
+> python app.py
+```
+
+Para ver desde una perspectiva de depuración ir a la carpeta tb1util, Constants.py y editar la constante DEBUG = True. Permite ver el sistema de grid utilizado en el juego.
+
+## Integrantes
+
+  
+
+  
+
+- Manuel Augusto Alvarado Estanga (u201615101)
+
+- César Andrés López Hurtado (u201611112)
---
+++ app.py
@@ -0,0 +1,309 @@
+# import the pygame module, so you can use it
+import pygame
+from colorama import init,Back
+import csv
+from random import choice
+from os import path
+
+
+#TB1 Modules
+from tb1util.Constants import *
+from tb1util.Enums import GridItemType,Direction
+from tb1util.Spritesheet import SpriteSheet
+from Astar import astar,Node
+
+class Pasajero:
+    def __init__(self,end,distance):
+        self.end = end
+        self.distance=distance
+
+
+class GridItem:
+    def __init__(self,x,y,width,height):
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+
+class Player(GridItem):
+    def __init__(self,x,y,width,height,vel,spritesheet = None,direction = Direction.RIGHT, frameIndex = 0):
+        super().__init__(x,y,width,height)
+        self.vel=vel
+        self.direction = direction
+        self.frameIndex = frameIndex
+        self.spritesheet = spritesheet
+
+        if self.spritesheet is not None:
+            frames = []
+            for i in range(15):
+                frames.append((i*FRAME_SIZE,0,FRAME_SIZE,FRAME_SIZE))
+
+            self.images = [None] * 4
+            for dir in [Direction.LEFT,Direction.RIGHT,Direction.DOWN,Direction.UP]:
+                a = dir * 3
+                b = a + 3
+                self.images[dir] = self.spritesheet.images_at(frames[a:b],PLAYER_COLORKEY)
+
+    def move(self):
+        if self.direction == Direction.UP:
+            self.y -= self.vel
+        elif self.direction == Direction.DOWN:
+            self.y += self.vel
+        elif self.direction == Direction.LEFT:
+            self.x -= self.vel
+        elif self.direction == Direction.RIGHT:
+            self.x += self.vel
+
+        if self.frameIndex < 2:
+            self.frameIndex += 1
+        else:
+            self.frameIndex = 0
+
+    def stop(self):
+        self.frameIndex = 0
+
+    def blit_on(self,screen,debug = False):
+        if self.images is not None:
+            screen.blit(self.images[self.direction][self.frameIndex],(self.x, self.y))
+        if debug:
+            pygame.draw.rect(screen,COLOR_BLUE,(self.x,self.y,FRAME_SIZE,FRAME_SIZE),1)
+
+
+def printPath(scenario):
+    init(convert=True)
+    n = len(scenario)
+    for i in range(n):
+        k = len(scenario[i])
+        for j in range(k):
+            value = scenario[i][j]
+            background = Back.BLACK
+            if value == 10:
+                background = Back.MAGENTA
+            elif value == 11:
+                background = Back.CYAN
+            elif value == 2:
+                background = Back.BLUE
+            elif value == 3:
+                background = Back.GREEN
+            elif value == 4:
+                background = Back.RED
+            print(background + '  ', end = '')
+        print('')
+
+class Game:
+    def __init__(self):
+        self.gameover = False
+        self.player = None
+        self.readScenario()
+        self.nuevos = None
+        self.pos_actual = None
+
+
+    def calcular_distancias_nuevos(self):
+        if len(self.nuevos) >= 1:
+            for pas in self.nuevos:
+                _end =  Node(None, pas.end)
+                _pa =  Node(None, self.pos_actual)
+                pas.distance = Node.Manhattan(_pa,_end)
+            self.nuevos.sort(key = lambda x: x.distance)
+
+    #def obtener_destino(self):
+
+
+
+    #obtener datos del csv
+    def readScenario(self,level = 1):
+        with open(path.join('resources','maps',f'map{level}.csv'),'r') as csvFile:
+            reader = csv.reader(csvFile)
+            self.scenario = [[0 for i in range(N_FRAMES)] for j in range(N_FRAMES)]
+            for i,row in enumerate(reader):
+                for j,gridValue in enumerate(row):
+                    pos_x = i * FRAME_SIZE
+                    pos_y = j * FRAME_SIZE
+                    self.scenario[i][j] = (int(gridValue),pygame.Rect(pos_x,pos_y,FRAME_SIZE,FRAME_SIZE))
+        csvFile.close()
+
+    #
+    def getScenarioRects(self):
+        n = N_FRAMES ** 2
+        num_columns = N_FRAMES
+        rects = [None] * n
+        for index in range(n):
+            x = index % num_columns
+            y = index // num_columns
+            _,rects[index] = self.scenario[x][y]
+        return rects
+
+
+    def getCoordsFromScenarioRectsIndex(self,rectIndex):
+        num_columns = N_FRAMES
+        x = rectIndex % num_columns
+        y = rectIndex // num_columns
+        return (x,y)
+
+
+    def getValueMatrix(self):
+        matrix = [[0 for i in range(N_FRAMES)] for j in range(N_FRAMES)]
+        for i in range(N_FRAMES):
+            for j in range(N_FRAMES):
+                matrix[i][j],_ = self.scenario[j][i]
+        return matrix
+
+    def printScenario(self,debug = False):
+        print([item.distance for item in self.nuevos])
+        for i in range(N_FRAMES):
+            for j in range(N_FRAMES):
+                value,rect = self.scenario[i][j]
+                if value == GridItemType.ROAD:
+                    pygame.draw.rect(self.screen,COLOR_ROAD,rect)
+                elif value == GridItemType.GROUND:
+                    pygame.draw.rect(self.screen,COLOR_GREEN,rect)
+                elif value == GridItemType.SEMAPH_GREEN:
+                    self.screen.blit(self.semaph_green,(rect.x,rect.y))
+                elif value == GridItemType.SEMAPH_RED:
+                    self.screen.blit(self.semaph_red,(rect.x,rect.y))
+                elif value == GridItemType.TARGET:
+                    pygame.draw.rect(self.screen,COLOR_SKYBLUE,rect)
+                if debug:
+                    pygame.draw.rect(self.screen,COLOR_RED,rect,1)
+
+    def preload(self):
+        #Init game
+        pygame.init()
+        self.screen = pygame.display.set_mode((GAME_WIDTH,GAME_HEIGHT))
+
+
+        #Load resources
+        self.logo = pygame.image.load(path.join('resources','logo.png'))
+        self.semaph_red = pygame.image.load(path.join('resources','semaphore-red.png'))
+        self.semaph_green = pygame.image.load(path.join('resources','semaphore-green.png'))
+        self.ss_player = SpriteSheet(path.join('resources','player.png'))
+
+        #Create instances
+        self.pos_actual = (0*FRAME_SIZE,0*FRAME_SIZE)
+        self.player=Player(0*FRAME_SIZE,0*FRAME_SIZE,FRAME_SIZE,FRAME_SIZE,4,self.ss_player)
+
+        self.nuevos = []
+        pygame.display.set_icon(self.logo)
+        pygame.display.set_caption("Grin Route")
+
+    def create(self):
+        #First draw
+        self.screen.fill(SCREEN_BACKGROUND_COLOR)
+        self.printScenario(DEBUG)
+        pygame.display.update()
+
+    def calculatePath(self,x,y, path):
+        scenario = self.getValueMatrix()
+        start = (self.player.y // FRAME_SIZE,self.player.x // FRAME_SIZE)
+        p,evaluated = astar(scenario,start,(y,x),Node.Manhattan)
+        npath = len(p)
+        for i in range(npath - 1):
+            y1,x1 = p[i]
+            y2,x2 = p[i+1]
+            difx = x2 - x1
+            dify = y2 - y1
+            #agregar mov del jugador
+            if difx > 0:
+                path.append(Direction.RIGHT)
+            elif difx < 0:
+                path.append(Direction.LEFT)
+            elif dify > 0:
+                path.append(Direction.DOWN)
+            elif dify < 0:
+                path.append(Direction.UP)
+
+        for step in evaluated:
+            i,j = step
+            scenario[i][j] = 11
+        for step in p:
+            i,j = step
+            scenario[i][j] = 10
+        printPath(scenario)
+
+    def movimiento(self, path):
+        currentTarget = None
+
+        scenarioRects = self.getScenarioRects()
+        mouseRect = pygame.Rect(int(self.nuevos[0].end[0]/32)*32,int(self.nuevos[0].end[1]/32)*32,FRAME_SIZE,FRAME_SIZE)
+        collideIndex = mouseRect.collidelist(scenarioRects)
+        self.pos_actual = self.getCoordsFromScenarioRectsIndex(collideIndex)
+        #movimiento
+        x,y = self.pos_actual
+        #print(f'target: {x,y}')
+        gridType,_ = self.scenario[x][y]
+        #print(f'gridType: {gridType}')
+
+        if gridType is GridItemType.ROAD:
+            _,r = self.scenario[x][y]
+            self.scenario[x][y] = (GridItemType.TARGET,r)
+            if currentTarget is not None:
+                a,b = currentTarget
+                _,d = self.scenario[a][b]
+                self.scenario[a][b] = (1,d)
+            currentTarget = (x,y)
+
+        self.calculatePath(self.pos_actual[0],self.pos_actual[1],path)
+
+
+
+
+    def update(self):
+        path = []
+        c = 1
+        move = None
+        ini = False
+        while not self.gameover:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.gameover = True
+                # click event and get the position
+                if event.type ==pygame.MOUSEBUTTONDOWN:
+                    """
+                    if len(self.nuevos) >= 1:
+                        path = []
+                    """
+                    ini = True
+                    mx,my=pygame.mouse.get_pos()
+                    mx=int(mx/32)*32
+                    my=int(my/32)*32
+                    final = (mx,my)
+                    pasjeroNuevo = Pasajero(final,0)
+                    self.nuevos.append(pasjeroNuevo)
+                    self.calcular_distancias_nuevos()
+                    self.calculatePath(self.pos_actual[0],self.pos_actual[1],path)
+                if ini:
+                    self.movimiento(path)
+                    if len(path) == 0:
+                        self.nuevos.pop(0)
+                else:
+                    ini = False
+            #keys = pygame.key.get_pressed()
+
+
+            self.screen.fill(SCREEN_BACKGROUND_COLOR)
+            self.printScenario(DEBUG)
+            self.player.blit_on(self.screen,DEBUG)
+            if c == 9:
+                if len(path) > 0:
+                    move = path.pop(0)
+                else:
+                    move = None
+                c = 1
+            if move is not None:
+                self.player.direction = move
+                self.player.move()
+            c += 1
+            pygame.display.update()
+
+
+def main():
+    game = Game()
+    game.preload()
+    game.create()
+    game.update()
+
+
+if __name__=="__main__":
+    main()
